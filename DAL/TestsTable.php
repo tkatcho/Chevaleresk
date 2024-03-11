024afb039ea52e7b5d16996ea69ad8dedad6f286
thomas-lavoie <202142181@edu.clg.qc.ca>
2024-03-11 09:27:26 -0400

    Master pages etc...

---
+++ DAL/TestsTable.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'DAL/models/test.php';
+include_once "DAL/MySQLDataBase.php";
+
+final class TestsTable extends MySQLTable
+{
+    public function __construct()
+    {
+        parent::__construct(DB(), new Test());
+    }
+    public function aliasExist($alias)
+    {
+        $user = $this->selectWhere("alias = '$alias'");
+        return isset($user[0]);
+    }
+    public function findByAlias($alias)
+    {
+        $user = $this->selectWhere("alias = '$alias'");
+        if (isset($user[0]))
+            return $user[0];
+        return null;
+    }
+    public function insert($test)
+    {
+        parent::insert($test);
+    }
+    public function update($test)
+    {
+        $userToUpdate = $this->get($test->Id);
+        if ($test->Password == "")
+            $test->Password = $userToUpdate->Password;
+        if ($userToUpdate != null) {
+            parent::update($test);
+        }
+    }
+    public function delete($id)
+    {
+        $userToRemove = $this->get($id);
+        if ($userToRemove != null) {
+            return parent::delete($id);
+        }
+        return false;
+    }
+}
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+    include 'DAL/ChevalereskDB.php';
+
+    $results = TestsTable()->selectAll();
+
+    $tableDesTests = "";
+                
+    foreach ($results as $result) {
+        $id = $result->Id;
+        $alias = $result->Alias;
+        $prenom = $result->Prenom;
+        $nom = $result->Nom;
+
+        $tableDesTests .= <<<HTML
+            <tr>
+                <td>$id</td>
+                <td>$alias</td>
+                <td>$prenom</td>
+                <td>$nom</td>
+            </tr>
+        HTML;
+    }
+
+    $title = "Test";
+
+    $content = <<<HTML
+        <h1>Test de la BD</h1>
+        <table class="table">
+            <tr>
+                <th>Id</th>
+                <th>Alias</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+            </tr>
+            $tableDesTests
+        </table>
+    HTML;
+
+    include 'views/master.php';
