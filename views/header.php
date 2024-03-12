2ba692e70b02e39c53bef9e1a70d777be7532e02
xxrosaliexx <rosalie.boyer@icloud.com>
2024-03-12 08:33:54 -0400

    header

---
+++ views/header.php
@@ -0,0 +1,23 @@
+<?php
+    $pageTitle = "Catalogue de produits";
+    if(!isset($viewTitle))
+        $viewTitle="";
+    if(!isset($viewHeadCustom))
+        $viewHeadCustom="";
+    if (!isset($viewName))
+        $viewName = "";
+
+    $loggedPlayerMenu = "";
+    
+    //Si le joueur est connecté
+    if (isset($_SESSION["validPlayer"]) ) {
+        $playerAlias = $_SESSION["playerAlias"];
+        
+       // $loggedPlayerMenu = <<<HTML
+        
+           
+    }else {  //si le joueur n'est pas connecté
+        $loggedUserMenu =<<<HTML
+        <a href ="loginForm.php">
+            <i class
+    }
