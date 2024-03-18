4ea51f22edafbf6b284cec3b8a39770b2b50a98f
xxrosaliexx <rosalie.boyer@icloud.com>
2024-03-18 10:16:44 -0400

    css

--- optionsJeu.php
+++ optionsJeu.php
@@ -4,9 +4,9 @@ include 'DAL/ChevalereskDB.php';
 
 
 $viewTitle="Bienvenue à Chevaleresk";
-$content= <<<HTML
+/*$content= <<<HTML
   
-  <!------------------------------------------------------------------------->
+  <!---------------------------Options pour joueur-------------------------------->
   <!--Le profil à droite-->
   <div class="optionsJeu">
     <div class="optionsBackgroundGrisProfil">
@@ -21,17 +21,77 @@ $content= <<<HTML
     <!--Les options à gauche-->
     <div class="optionsBtn" >
         <button>
-            <a href ="loginForm.php">
-            Achat <i class="fa fa-user"></i>
+            <a class="optionsBtnIcon" href ="loginForm.php">
+            Achat <i class="fa fa-money"></i>
             </a>
         </button>
         <button>
-            <a href ="subscribeForm.php">
-            S'inscrire <i class="fa fa-sign-in"></i>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Inventaire <i class="fa fa-id-card-o"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Modifier Profil <i class="fa fa-user"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Gagner plus d'argent <i class="fa fa-gift"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Déconnexion <i class="fa fa-sign-out"></i>
             </a>
         </button>
     </div>
 </div>
-HTML;
+
+
+<!---------------------------Options pour admin-------------------------------->
+  <!--Le profil à droite-->
+  <div class="optionsJeu">
+    <div class="optionsBackgroundGrisProfil">
+        <strong>Profil</strong>
+        <div class="optionsBackgroundBleuProfilImg">
+            <div style="background-image:url('./images/chevalier.png')"></div>
+        </div>
+        <p>Admin</p>
+        
+    </div>
+    
+    <!--Les options à gauche-->
+    <div class="optionsBtn" >
+        <button>
+            <a class="optionsBtnIcon" href ="loginForm.php">
+            Achat <i class="fa fa-money"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Inventaire <i class="fa fa-id-card-o"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Modifier Profil <i class="fa fa-user"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Gagner plus d'argent <i class="fa fa-gift"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Déconnexion <i class="fa fa-sign-out"></i>
+            </a>
+        </button>
+    </div>
+</div>
+HTML;*/
+
+$content = <<<HTML
 
 include 'views/master.php';
