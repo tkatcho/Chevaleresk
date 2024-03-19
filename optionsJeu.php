53a01ddf8a6e60fd1e7c01ae1bc1731873575fdb
xxrosaliexx <rosalie.boyer@icloud.com>
2024-03-19 08:19:19 -0400

    css

---
+++ optionsJeu.php
@@ -0,0 +1,141 @@
+<?php
+
+include 'DAL/ChevalereskDB.php';
+
+
+$viewTitle="Bienvenue à Chevaleresk";
+$isConnected= isset($_SESSION['validUser']) && $_SESSION['validUser'];
+/*$content= <<<HTML
+  
+  <!---------------------------Options pour joueur-------------------------------->
+  <!--Le profil à droite-->
+  <div class="optionsJeu">
+    <div class="optionsBackgroundGrisProfil">
+        <strong>Profil</strong>
+        <div class="optionsBackgroundBleuProfilImg">
+            <div style="background-image:url('./images/chevalier.png')"></div>
+        </div>
+        <p>Alias</p>
+        <p>Nombre écus: <span>1000</span>$</p> 
+    </div>
+    
+    <!--Les options à gauche-->
+    <div class="optionsBtn" >
+        <button>
+            <a class="optionsBtnIcon" href ="loginForm.php">
+            Achat <i class="fa fa-money"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Inventaire <i class="fa fa-id-card-o"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Modifier Profil <i class="fa fa-user"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Gagner plus d'argent <i class="fa fa-gift"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Déconnexion <i class="fa fa-sign-out"></i>
+            </a>
+        </button>
+    </div>
+</div>
+
+
+<!---------------------------Options pour admin-------------------------------->
+  <!--Le profil à droite-->
+  <div class="optionsJeu">
+    <div class="optionsBackgroundGrisProfil">
+        <strong>Profil</strong>
+        <div class="optionsBackgroundBleuProfilImg">
+            <div style="background-image:url('./images/chevalier.png')"></div>
+        </div>
+        <p>Admin</p>
+        
+    </div>
+    
+    <!--Les options à gauche-->
+    <div class="optionsBtn" >
+        <button>
+            <a class="optionsBtnIcon" href ="loginForm.php">
+            Achat <i class="fa fa-money"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Inventaire <i class="fa fa-id-card-o"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Modifier Profil <i class="fa fa-user"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Gagner plus d'argent <i class="fa fa-gift"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Déconnexion <i class="fa fa-sign-out"></i>
+            </a>
+        </button>
+    </div>
+</div>
+HTML;*/
+//if($isConnected){
+    $content = <<<HTML
+    <!--Le profil à droite-->
+    <div class="optionsJeu">
+      <div class="optionsBackgroundGrisProfil">
+          <strong>Profil</strong>
+          <div class="optionsBackgroundBleuProfilImg">
+              <div style="background-image:url('./images/chevalier.png')"></div>
+          </div>
+          <p>Alias</p>
+          <p>Nombre écus: <span>1000</span>$</p> 
+      </div>
+      
+      <!--Les options à gauche-->
+      <div class="optionsBtn" >
+          <button>
+              <a class="optionsBtnIcon" href ="loginForm.php">
+              Achat <i class="fa fa-money"></i>
+              </a>
+          </button>
+          <button>
+              <a class="optionsBtnIcon" href ="subscribeForm.php">
+              Inventaire <i class="fa fa-id-card-o"></i>
+              </a>
+          </button>
+          <button>
+              <a class="optionsBtnIcon" href ="subscribeForm.php">
+              Modifier Profil <i class="fa fa-user"></i>
+              </a>
+          </button>
+          <button>
+              <a class="optionsBtnIcon" href ="subscribeForm.php">
+              Gagner plus d'argent <i class="fa fa-gift"></i>
+              </a>
+          </button>
+          <button>
+              <a class="optionsBtnIcon" href ="subscribeForm.php">
+              Déconnexion <i class="fa fa-sign-out"></i>
+              </a>
+          </button>
+      </div>
+  </div>
+  
+  HTML;
+//}
+
+include 'views/master.php';
