6b326667f307ae3cffb8ecbf1a0310e936c897c8
thomas-lavoie <202142181@edu.clg.qc.ca>
2024-03-19 15:24:16 -0400

    fix procedure addPanier et achat du panier fonctionnel

--- addToCart.php
+++ addToCart.php
@@ -3,7 +3,6 @@
 include 'DAL/ChevalereskDB.php';
 include 'php/sessionManager.php';
 
-
 if (isset($_GET['idJoueur']) && isset($_GET['idItem']) && isset($_GET['qt']))
 {
     DB()->nonQuerySqlCmd("CALL addPanier($_GET[idJoueur], $_GET[idItem], $_GET[qt]);");
--- procedures.sql
+++ procedures.sql
@@ -38,13 +38,15 @@ BEGIN
     SELECT quantiteStock INTO qt_stock FROM items WHERE id = id_item;
     SELECT COUNT(*) INTO item_exists FROM paniers WHERE idJoueur = id_joueur AND idItem = id_item;
     
-    IF (qt > 0) THEN
-        IF (item_exists > 0) THEN
-            IF (qt_stock >= (SELECT quantite FROM paniers WHERE idItem = id_item AND idJoueur = id_joueur) + qt) THEN
-                UPDATE paniers SET quantite = quantite + qt WHERE idJoueur = id_joueur AND idItem = id_item;
+    IF (qt_stock > 0) THEN
+        IF (qt > 0) THEN
+            IF (item_exists > 0) THEN
+                IF (qt_stock >= (SELECT quantite FROM paniers WHERE idItem = id_item AND idJoueur = id_joueur) + qt) THEN
+                    UPDATE paniers SET quantite = quantite + qt WHERE idJoueur = id_joueur AND idItem = id_item;
+                END IF;
+            ELSE
+                INSERT INTO paniers (idJoueur, idItem, quantite) VALUES (id_joueur, id_item, qt);
             END IF;
-        ELSE
-            INSERT INTO paniers (idJoueur, idItem, quantite) VALUES (id_joueur, id_item, qt);
         END IF;
     END IF;
 END//
@@ -53,7 +55,6 @@ DELIMITER ;
 
 
 -- Acheter le panier
--- FONCTIONNE PAS!!!
 DELIMITER //
 
 CREATE PROCEDURE buyPanier(IN id_joueur INT)
@@ -61,23 +62,34 @@ BEGIN
     DECLARE total INT;
     DECLARE item_id_item INT;
     DECLARE item_quantite INT;
-    DECLARE itemCr CURSOR FOR SELECT idItem, quantite FROM paniers WHERE idJoueur = id_joueur;
+    DECLARE done INT;
+
+    DECLARE cur CURSOR FOR SELECT idItem, quantite FROM paniers WHERE idJoueur = id_joueur;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
     SELECT SUM(quantite * prix) INTO total FROM paniers INNER JOIN items ON paniers.idItem = items.id WHERE idJoueur = id_joueur;
-    
-    OPEN itemCr;
-    FETCH itemCr INTO item_id_item, item_quantite;
-    itemLoop: LOOP
-        IF (item_quantite <= (SELECT quantiteStock FROM items WHERE id = item_id_item)) THEN
-            UPDATE items SET quantiteStock = (quantiteStock - item_quantite) WHERE id = item_id_item;
+
+    IF (total <= (SELECT solde FROM joueurs WHERE id = id_joueur)) THEN
+        OPEN cur ;
+            update_loop: LOOP
+                FETCH cur INTO item_id_item, item_quantite;
+                    IF (done = 1) THEN
+                        LEAVE update_loop;
+                    END IF;
+                UPDATE items SET quantiteStock = quantiteStock - item_quantite WHERE id = item_id_item;
+                IF ((SELECT COUNT(*) FROM inventaires WHERE idJoueur = id_joueur AND idItem = item_id_item) = 0) THEN
+                    INSERT INTO inventaires (idJoueur, idItem, quantite) VALUES (id_joueur, item_id_item, item_quantite);
+                ELSE
+                    UPDATE inventaires SET quantite = quantite + item_quantite WHERE idJoueur = id_joueur AND idItem = item_id_item;
+                END IF;
+                DELETE FROM paniers WHERE idJoueur = id_joueur AND idItem = item_id_item;
+            END LOOP update_loop ;
+        CLOSE cur ;
+
+        IF (total IS NOT NULL) THEN
+            UPDATE joueurs SET solde = solde - total WHERE id = id_joueur;
         END IF;
-        FETCH itemCr INTO item_id_item, item_quantite;
-        IF (item_id_item IS NULL) THEN
-            LEAVE itemLoop;
-        END IF;
-        FETCH itemCr INTO item_id_item, item_quantite;
-    END LOOP itemLoop;
-    CLOSE itemCr;
+    END IF ;
 END//
 
 DELIMITER ;
