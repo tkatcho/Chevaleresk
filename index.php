2541c724a231d2f75a0e4273c9224fd0fbc39657
thomas-lavoie <202142181@edu.clg.qc.ca>
2024-03-18 22:19:19 -0400

    protection si items est null

--- index.php
+++ index.php
@@ -383,172 +383,175 @@ HTML;
 
 $index = 1;
 $items = ItemsTable()->selectAll();
-foreach ($items as $item) {
+if ($items != null) {
+    foreach ($items as $item) {
 
-    $addToCartBouton = "";
-    if ($isConnected)
-        $addToCartBouton = addToCartButton($_SESSION['id'], $item->Id, 1);
+        $addToCartBouton = "";
+        if ($isConnected)
+            $addToCartBouton = addToCartButton($_SESSION['id'], $item->Id, 1);
 
-    if ($item->Type == 'p') { // Potions
-        $potion = PotionsTable()->selectWhere("idItem = $item->Id")[0];
-        $type = "Défence";
-        if ($potion->estAttaque)
-            $type = "Attaque";
-        $itemsDisplay .= <<<HTML
-            <div class="containerItem">
-                <span class="idItem">$index</span> 
-                $item->Nom
-                <span>
-                    $addToCartBouton
-                </span>
-                <hr>
-                <div class="itemImage">
-                    <div style="background-image:url('$item->Photo')"></div>
+        if ($item->Type == 'p') { // Potions
+            $potion = PotionsTable()->selectWhere("idItem = $item->Id")[0];
+            $type = "Défence";
+            if ($potion->estAttaque)
+                $type = "Attaque";
+            $itemsDisplay .= <<<HTML
+                <div class="containerItem">
+                    <span class="idItem">$index</span> 
+                    $item->Nom
+                    <span>
+                        $addToCartBouton
+                    </span>
+                    <hr>
+                    <div class="itemImage">
+                        <div style="background-image:url('$item->Photo')"></div>
+                    </div>
+                    <hr>
+                    <p>Type item: 
+                        <span>Potion</span>
+                    </p>
+                    <p>Effet: 
+                        <span>$potion->Effet</span>
+                    </p>
+                    <p>Durée: 
+                        <span>$potion->Duree</span>
+                    </p>
+                    <p>Type: 
+                        <span>$type</span>
+                    </p>
+                    <hr>
+                    <p>Quantité en stock:
+                        <span>$item->QuantiteStock</span>
+                    </p>
+                    <hr>
+                    <p class="itemPrix">Prix: 
+                        <span>$item->Prix</span> $
+                    <p>
                 </div>
-                <hr>
-                <p>Type item: 
-                    <span>Potion</span>
-                </p>
-                <p>Effet: 
-                    <span>$potion->Effet</span>
-                </p>
-                <p>Durée: 
-                    <span>$potion->Duree</span>
-                </p>
-                <p>Type: 
-                    <span>$type</span>
-                </p>
-                <hr>
-                <p>Quantité en stock:
-                    <span>$item->QuantiteStock</span>
-                </p>
-                <hr>
-                <p class="itemPrix">Prix: 
-                    <span>$item->Prix</span> $
-                <p>
-            </div>
-        HTML;
-    }
-    
-    if ($item->Type == 'w') { // Armes
-        $arme = ArmesTable()->selectWhere("idItem = $item->Id")[0];
-        $itemsDisplay .= <<<HTML
-            <div class="containerItem">
-                <span class="idItem">$index</span> 
-                $item->Nom
-                <span>
-                    $addToCartBouton
-                </span>
-                <hr>
-                <div class="itemImage">
-                    <div style="background-image:url('./images/épée.png')"></div>
+            HTML;
+        }
+        
+        if ($item->Type == 'w') { // Armes
+            $arme = ArmesTable()->selectWhere("idItem = $item->Id")[0];
+            $itemsDisplay .= <<<HTML
+                <div class="containerItem">
+                    <span class="idItem">$index</span> 
+                    $item->Nom
+                    <span>
+                        $addToCartBouton
+                    </span>
+                    <hr>
+                    <div class="itemImage">
+                        <div style="background-image:url('./images/épée.png')"></div>
+                    </div>
+                    <hr>
+                    <p>Type item: 
+                        <span>Arme</span>
+                    </p>
+                    <p>Efficacité: 
+                        <span>$arme->Efficacite</span>
+                    </p>
+                    <p>Genre: 
+                        <span>$arme->Genre</span>
+                    </p>
+                    <p>Description:
+                    <span>$arme->Description</span>
+                    </p>
+                    <hr>
+                    <p>Quantité en stock: 
+                        <span>$item->QuantiteStock</span>
+                    </p>
+                    <hr>
+                    <p class="itemPrix">Prix: 
+                        <span>$item->Prix</span> $
+                    <p>
                 </div>
-                <hr>
-                <p>Type item: 
-                    <span>Arme</span>
-                </p>
-                <p>Efficacité: 
-                    <span>$arme->Efficacite</span>
-                </p>
-                <p>Genre: 
-                    <span>$arme->Genre</span>
-                </p>
-                <p>Description:
-                <span>$arme->Description</span>
-                </p>
-                <hr>
-                <p>Quantité en stock: 
-                    <span>$item->QuantiteStock</span>
-                </p>
-                <hr>
-                <p class="itemPrix">Prix: 
-                    <span>$item->Prix</span> $
-                <p>
-            </div>
-        HTML;
-    }
+            HTML;
+        }
 
-    if ($item->Type == 'a') { // Armures
-        $armure = ArmuresTable()->selectWhere("idItem = $item->Id")[0];
-        $itemsDisplay .= <<<HTML
-            <div class="containerItem">
-                <span class="idItem">$index</span> 
-                $item->Nom
-                <span>
-                    $addToCartBouton
-                </span>
-                <hr>
-                <div class="itemImage">
-                    <div style="background-image:url('$item->Photo')"></div>
+        if ($item->Type == 'a') { // Armures
+            $armure = ArmuresTable()->selectWhere("idItem = $item->Id")[0];
+            $itemsDisplay .= <<<HTML
+                <div class="containerItem">
+                    <span class="idItem">$index</span> 
+                    $item->Nom
+                    <span>
+                        $addToCartBouton
+                    </span>
+                    <hr>
+                    <div class="itemImage">
+                        <div style="background-image:url('$item->Photo')"></div>
+                    </div>
+                    <hr>
+                    <p>Type item: 
+                        <span>Armure</span>
+                    </p>
+                    <p>Matière: 
+                        <span>$armure->Matiere</span>
+                    </p>
+                    <p>Taille: 
+                        <span>$armure->Taille</span>
+                    </p>
+                    
+                    <hr>
+                    <p>Quantité en stock: 
+                        <span>$item->QuantiteStock</span>
+                    </p>
+                    <hr>
+                    <p class="itemPrix">Prix: 
+                        <span>$item->Prix</span> $
+                    <p>
                 </div>
-                <hr>
-                <p>Type item: 
-                    <span>Armure</span>
-                </p>
-                <p>Matière: 
-                    <span>$armure->Matiere</span>
-                </p>
-                <p>Taille: 
-                    <span>$armure->Taille</span>
-                </p>
-                
-                <hr>
-                <p>Quantité en stock: 
-                    <span>$item->QuantiteStock</span>
-                </p>
-                <hr>
-                <p class="itemPrix">Prix: 
-                    <span>$item->Prix</span> $
-                <p>
-            </div>
-        HTML;
-    }
+            HTML;
+        }
 
-    if ($item->Type == 'e') { // Éléments
-        $element = ElementsTable()->selectWhere("idItem = $item->Id")[0];
-        $itemsDisplay .= <<<HTML
-            <div class="containerItem">
-                <span class="idItem">5</span> 
-                $item->Nom
-                <span>
-                    $addToCartBouton
-                </span>
-                <hr>
-                <div class="itemImage">
-                    <div  style="background-image:url('$item->Photo')"></div>
+        if ($item->Type == 'e') { // Éléments
+            $element = ElementsTable()->selectWhere("idItem = $item->Id")[0];
+            $itemsDisplay .= <<<HTML
+                <div class="containerItem">
+                    <span class="idItem">5</span> 
+                    $item->Nom
+                    <span>
+                        $addToCartBouton
+                    </span>
+                    <hr>
+                    <div class="itemImage">
+                        <div  style="background-image:url('$item->Photo')"></div>
+                    </div>
+                    <hr>
+                    <p>Type item: 
+                        <span>Élément</span>
+                    </p>
+                    <p>Type: 
+                        <span>$element->Type</span>
+                    </p>
+                    <p>Rareté: 
+                        <span>$element->Rarete</span>
+                    </p>
+                    <p>Dangerosité: 
+                        <span>$element->Dangerosite</span>
+                    </p>
+                    
+                    <hr>
+                    <p>Quantité en stock: 
+                        <span>$item->QuantiteStock</span>
+                    </p>
+                    <hr>
+                    <p class="itemPrix">Prix: 
+                        <span>$item->Prix</span> $
+                    <p>
                 </div>
-                <hr>
-                <p>Type item: 
-                    <span>Élément</span>
-                </p>
-                <p>Type: 
-                    <span>$element->Type</span>
-                </p>
-                <p>Rareté: 
-                    <span>$element->Rarete</span>
-                </p>
-                <p>Dangerosité: 
-                    <span>$element->Dangerosite</span>
-                </p>
-                
-                <hr>
-                <p>Quantité en stock: 
-                    <span>$item->QuantiteStock</span>
-                </p>
-                <hr>
-                <p class="itemPrix">Prix: 
-                    <span>$item->Prix</span> $
-                <p>
-            </div>
-        HTML;
-    }
+            HTML;
+        }
 
-    $index++;
+        $index++;
+    }
 }
 
 $itemsDisplay .= <<<HTML
     </div>
 HTML;
+
 
 $content .= $itemsDisplay;
 
