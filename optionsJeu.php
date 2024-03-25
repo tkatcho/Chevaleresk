39887c5f488ce76461bfcb4d6be9e475026ab97d
xxrosaliexx <rosalie.boyer@icloud.com>
2024-03-25 09:19:26 -0400

    optionsAdminFait

--- optionsJeu.php
+++ optionsJeu.php
@@ -5,10 +5,11 @@ include 'DAL/ChevalereskDB.php';
 
 $viewTitle="Bienvenue à Chevaleresk";
 
-
+if((bool)$_SESSION["isAdmin"] && isset($_SESSION['isAdmin'])){
+adminAccess();
 $content= <<<HTML
   
-  <!---------------------------Options pour joueur-------------------------------->
+  <!---------------------------Options pour admin-------------------------------->
   <!--Le profil à droite-->
   <div class="optionsJeu">
     <div class="optionsBackgroundGrisProfil">
@@ -17,72 +18,21 @@ $content= <<<HTML
             <div style="background-image:url('./images/chevalier.png')"></div>
         </div>
         <p>Alias</p>
-        <p>Nombre écus: <span>1000</span>$</p> 
+       
     </div>
     
     <!--Les options à gauche-->
     <div class="optionsBtn" >
+       
         <button>
-            <a class="optionsBtnIcon" href ="loginForm.php">
-            Achat <i class="fa fa-money"></i>
+            <a class="optionsBtnIcon" href ="newItem.php">
+            Nouveau item <i class="fa fa-id-card-o"></i>
             </a>
         </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Inventaire <i class="fa fa-id-card-o"></i>
-            </a>
-        </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Modifier Profil <i class="fa fa-user"></i>
-            </a>
-        </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Gagner plus d'argent <i class="fa fa-gift"></i>
-            </a>
-        </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Déconnexion <i class="fa fa-sign-out"></i>
-            </a>
-        </button>
-    </div>
-</div>
-
-
-<!---------------------------Options pour admin-------------------------------->
-  <!--Le profil à droite-->
-  <div class="optionsJeu">
-    <div class="optionsBackgroundGrisProfil">
-        <strong>Profil</strong>
-        <div class="optionsBackgroundBleuProfilImg">
-            <div style="background-image:url('./images/chevalier.png')"></div>
-        </div>
-        <p>Admin</p>
         
-    </div>
-    
-    <!--Les options à gauche-->
-    <div class="optionsBtn" >
-        <button>
-            <a class="optionsBtnIcon" href ="loginForm.php">
-            Achat <i class="fa fa-money"></i>
-            </a>
-        </button>
         <button>
             <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Inventaire <i class="fa fa-id-card-o"></i>
-            </a>
-        </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Modifier Profil <i class="fa fa-user"></i>
-            </a>
-        </button>
-        <button>
-            <a class="optionsBtnIcon" href ="subscribeForm.php">
-            Gagner plus d'argent <i class="fa fa-gift"></i>
+            Argent <i class="fa fa-gift"></i>
             </a>
         </button>
         <button>
@@ -92,9 +42,13 @@ $content= <<<HTML
         </button>
     </div>
 </div>
-HTML;*/
+HTML;
+} else{
 
-$content = <<<HTML
+$content=<<<HTML
+<!---------------------------Options pour joueur-------------------------------->
+
+
   <!--Le profil à droite-->
   <div class="optionsJeu">
     <div class="optionsBackgroundGrisProfil">
@@ -137,4 +91,5 @@ $content = <<<HTML
 </div>
 
 HTML;
+}
 include 'views/master.php';
