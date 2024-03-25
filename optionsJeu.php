bb1172d3a1d5ad7bf2f803cf5688e8fa0f21366e
xxrosaliexx <rosalie.boyer@icloud.com>
2024-03-25 09:21:59 -0400

    optionsJeu

--- optionsJeu.php
+++ optionsJeu.php
@@ -1,14 +1,15 @@
 <?php
 
 include 'DAL/ChevalereskDB.php';
-include 'php/sessionManager.php';
+
 
 $viewTitle="Bienvenue à Chevaleresk";
-$isConnected= isset($_SESSION['validUser']) && $_SESSION['validUser'];
 
-/*$content= <<<HTML
+if((bool)$_SESSION["isAdmin"] && isset($_SESSION['isAdmin'])){
+adminAccess();
+$content= <<<HTML
   
-<!---------------------------Options pour admin-------------------------------->
+  <!---------------------------Options pour admin-------------------------------->
   <!--Le profil à droite-->
   <div class="optionsJeu">
     <div class="optionsBackgroundGrisProfil">
@@ -16,8 +17,47 @@ $isConnected= isset($_SESSION['validUser']) && $_SESSION['validUser'];
         <div class="optionsBackgroundBleuProfilImg">
             <div style="background-image:url('./images/chevalier.png')"></div>
         </div>
-        <p>Admin</p>
+        <p>Alias</p>
+       
+    </div>
+    
+    <!--Les options à gauche-->
+    <div class="optionsBtn" >
+       
+        <button>
+            <a class="optionsBtnIcon" href ="newItem.php">
+            Nouveau item <i class="fa fa-id-card-o"></i>
+            </a>
+        </button>
         
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Argent <i class="fa fa-gift"></i>
+            </a>
+        </button>
+        <button>
+            <a class="optionsBtnIcon" href ="subscribeForm.php">
+            Déconnexion <i class="fa fa-sign-out"></i>
+            </a>
+        </button>
+    </div>
+</div>
+HTML;
+} else{
+
+$content=<<<HTML
+<!---------------------------Options pour joueur-------------------------------->
+
+
+  <!--Le profil à droite-->
+  <div class="optionsJeu">
+    <div class="optionsBackgroundGrisProfil">
+        <strong>Profil</strong>
+        <div class="optionsBackgroundBleuProfilImg">
+            <div style="background-image:url('./images/chevalier.png')"></div>
+        </div>
+        <p>Alias</p>
+        <p>Nombre écus: <span>1000</span>$</p> 
     </div>
     
     <!--Les options à gauche-->
@@ -49,59 +89,7 @@ $isConnected= isset($_SESSION['validUser']) && $_SESSION['validUser'];
         </button>
     </div>
 </div>
-HTML;*/
 
-if($isConnected){
-
-    //Faire une condition pour savoir si admin ou si joueur (les boutons ne seront pas les mêmes)
-    $joueur = JoueursTable()->selectById($_SESSION['id'])[0];
-
-    $content = <<<HTML
-    <!---------------------------Options pour joueur-------------------------------->
-    <!--Le profil à droite-->
-    <div class="optionsJeu">
-      <div class="optionsBackgroundGrisProfil">
-          <strong>Profil</strong>
-          <div class="optionsBackgroundBleuProfilImg">
-              <div style="background-image:url('./images/chevalier.png')"></div>
-          </div>
-          <p>$joueur->Alias</p>
-          <p>Nombre écus: <span>$joueur->Solde</span>$</p> 
-      </div>
-      
-      <!--Les options à gauche-->
-      <div class="optionsBtn" >
-          <button>
-              <a class="optionsBtnIcon" href ="index.php">
-              Achat <i class="fa fa-money"></i>
-              </a>
-          </button>
-          <button>
-              <a class="optionsBtnIcon" href ="inventaire.php">
-              Inventaire <i class="fa fa-id-card-o"></i>
-              </a>
-          </button>
-          <button>
-              <a class="optionsBtnIcon" href ="modifierProfil.php">
-              Modifier Profil <i class="fa fa-user"></i>
-              </a>
-          </button>
-          <button>
-              <a class="optionsBtnIcon" href ="optionsGagnerArgent.php">
-              Gagner plus d'argent <i class="fa fa-gift"></i>
-              </a>
-          </button>
-          <button>
-              <a class="optionsBtnIcon" href ="logout.php">
-              Déconnexion <i class="fa fa-sign-out"></i>
-              </a>
-          </button>
-      </div>
-  </div>
-  
-  HTML;
-}else {
-    redirect("index.php");
+HTML;
 }
-
 include 'views/master.php';
