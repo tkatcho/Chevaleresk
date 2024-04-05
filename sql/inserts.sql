d41aa01207ab7c0e8eb3adde1b77938678af8bb6
thomas-lavoie <202142181@edu.clg.qc.ca>
2024-04-05 09:29:07 -0400

    inserts

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -21,30 +21,30 @@ INSERT INTO items (nom, type, quantiteStock, prix, photo) VALUES ('Fangs', 'E', 
 INSERT INTO items (nom, type, quantiteStock, prix, photo) VALUES ('Flower', 'E', 10, 30, 'images/items/flower.png');
 
 -- Corresponding inserts for the Armes (Weapons) table, assuming Items.id = 1 for the first weapon, and so on
-INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM Items WHERE nom = 'Sword of Might'), 'High', 'Sword', 'A mighty sword that can split mountains.');
-INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM Items WHERE nom = 'Axe of Fury'), 'Medium', 'Axe', 'An axe that enrages its wielder to fight fiercely.');
-INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM Items WHERE nom = 'Dagger'), 'High', 'Sword', 'A dagger can kill.');
-INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM Items WHERE nom = 'Bow'), 'Medium', 'Bow', 'A pretty bow');
-INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM Items WHERE nom = 'Axe'), 'High', 'Axe', 'This axe is cool');
+INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM items WHERE nom = 'Sword of Might'), 'High', 'Sword', 'A mighty sword that can split mountains.');
+INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM items WHERE nom = 'Axe of Fury'), 'Medium', 'Axe', 'An axe that enrages its wielder to fight fiercely.');
+INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM items WHERE nom = 'Dagger'), 'High', 'Sword', 'A dagger can kill.');
+INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM items WHERE nom = 'Bow'), 'Medium', 'Bow', 'A pretty bow');
+INSERT INTO armes (idItem, efficacite, genre, description) VALUES ((SELECT id FROM items WHERE nom = 'Axe'), 'High', 'Axe', 'This axe is cool');
 
 -- Corresponding inserts for the Armures (Armor) table, assuming Items.id = 3 for the first armor, and so on
-INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM Items WHERE nom = 'Plate Armor'), 'Steel', 'L');
-INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM Items WHERE nom = 'Chainmail'), 'Iron', 'M');
-INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM Items WHERE nom = 'Helmet'), 'Steel', 'S');
-INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM Items WHERE nom = 'Fancy helmet'), 'Steel', 'S');
-INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM Items WHERE nom = 'Big armor'), 'Iron', 'L');
+INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM items WHERE nom = 'Plate Armor'), 'Steel', 'L');
+INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM items WHERE nom = 'Chainmail'), 'Iron', 'M');
+INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM items WHERE nom = 'Helmet'), 'Steel', 'S');
+INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM items WHERE nom = 'Fancy helmet'), 'Steel', 'S');
+INSERT INTO armures (idItem, matiere, taille) VALUES ((SELECT id FROM items WHERE nom = 'Big armor'), 'Iron', 'L');
 
 
 -- Corresponding inserts for the Potions table, assuming Items.id = 5 for the first potion, and so on
-INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM Items WHERE nom = 'Potion of Healing'), 'Restore Health', '00:00:30', 0);
-INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM Items WHERE nom = 'Potion of Strength'), 'Increase Strength', '00:00:20', 0);
-INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM Items WHERE nom = 'Potion of Poison'), 'Hurt Ennemies', '00:00:10', 1);
-INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM Items WHERE nom = 'Potion of High vision'), 'Increase Vision', '00:00:60', 0);
-INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM Items WHERE nom = 'Potion of Gold'), 'Increase Strength', '00:00:20', 0);
+INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM items WHERE nom = 'Potion of Healing'), 'Restore Health', '00:00:30', 0);
+INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM items WHERE nom = 'Potion of Strength'), 'Increase Strength', '00:00:20', 0);
+INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM items WHERE nom = 'Potion of Poison'), 'Hurt Ennemies', '00:00:10', 1);
+INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM items WHERE nom = 'Potion of High vision'), 'Increase Vision', '00:00:60', 0);
+INSERT INTO potions (idItem, effet, duree, estAttaque) VALUES ((SELECT id FROM items WHERE nom = 'Potion of Gold'), 'Increase Strength', '00:00:20', 0);
 
 -- Corresponding inserts for the Elements table, assuming Items.id = 7 for the first element, and so on
-INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM Items WHERE nom = 'Fire Essence'), 'Fire', 'Common', 'Low');
-INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM Items WHERE nom = 'Ice Shard'), 'Ice', 'Uncommon', 'Medium');
-INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM Items WHERE nom = 'Leaf'), 'Earth', 'Common', 'Low');
-INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM Items WHERE nom = 'Fangs'), 'Earth', 'Uncommon', 'Medium');
-INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM Items WHERE nom = 'Leaf'), 'Earth', 'Common', 'Low');
+INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM items WHERE nom = 'Fire Essence'), 'Fire', 'Common', 'Low');
+INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM items WHERE nom = 'Ice Shard'), 'Ice', 'Uncommon', 'Medium');
+INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM items WHERE nom = 'Leaf'), 'Earth', 'Common', 'Low');
+INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM items WHERE nom = 'Fangs'), 'Earth', 'Uncommon', 'Medium');
+INSERT INTO elements (idItem, type, rarete, dangerosite) VALUES ((SELECT id FROM items WHERE nom = 'Leaf'), 'Earth', 'Common', 'Low');
