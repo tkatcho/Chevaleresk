e041252903a4dba75009bd0884180c4446b8468e
thomas-lavoie <202142181@edu.clg.qc.ca>
2024-04-12 09:13:24 -0400

    procedures

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -183,16 +183,3 @@ BEGIN
 END//
 
 DELIMITER ;
-
-
-DELIMITER //
-
-CREATE PROCEDURE enigmeRepondu(IN idEnigme INT, IN idJoueur INT)
-BEGIN
-    DECLARE id_quete INT;
-    
-     SELECT id INTO id_quete FROM quetes INNER JOIN Enigmes ON paniers.idItem = items.id WHERE idJoueur = id_joueur;
-
-END//
-
-DELIMITER ;
