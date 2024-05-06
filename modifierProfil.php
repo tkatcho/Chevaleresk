635c5a3322c428bec31710c457fff019024ed90d
tkatcho <tkatcho@outlook.com>
2024-05-06 09:35:47 -0400

    Presque terminer modifier profil, ne reste que l'aspect style / message d'erreurs

---
+++ modifierProfil.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'DAL/ChevalereskDB.php';
+include 'php/sessionManager.php';
+
+if (isset($_POST['submit'])) {
+    $validUser = true;
+    $joueur;
+    if ($_POST['confirmPassword'] != $_POST['motDePasse']) {
+        $validUser = false;
+        redirect('signupForm.php?error=confirmPasswordFailed');
+    }
+
+    if (!JoueursTable()->aliasExist($_POST['alias']) || JoueursTable()->aliasExist($_POST['alias']) == $_SESSION['alias']) {
+        $joueur = JoueursTable()->findByAlias($_SESSION['alias']);
+        $joueur->Alias = $_POST['alias'];
+        $joueur->Nom = $_POST['nom'];
+        $joueur->Prenom = $_POST['prenom'];
+
+        if (strlen($_POST['motDePasse']) != 0) {
+            $joueur->MotDePasse = password_hash($_POST['motDePasse'], PASSWORD_DEFAULT);
+        } else {
+            $joueur->MotDePasse = "";
+        }
+
+        print_r($joueur);
+        JoueursTable()->update($joueur);
+        //ajouter min max requirements on html
+
+        //redirect('loginForm.php');
+    }
+
+    //redirect('loginForm.php');
+}
+
+//redirect('options.php');
